814f896c77139b57e98a36b209399c924cd2467b
zural <zural143@gmail.com>
2019-04-16 20:53:14 +0530

    dev++

--- server.js
+++ server.js
@@ -2,39 +2,33 @@ import express from 'express';
 
 import React from 'react';
 import ReactDOMServer from 'react-dom/server';
-
+import path from 'path'
 const app = express();
+app.use('/static', express.static(path.resolve(__dirname, 'public')))
 const PORT = process.env.PORT || 3000;
 
 import Home from './component';
 
-import bodyParser from 'body-parser';
+
 import { Helmet } from 'react-helmet';
 
-app.use(bodyParser.json());
-app.use(express.static(__dirname + '/build'));
-
-const renderFullPage = (html, helmet) => {
-  return `
-    <!DOCTYPE html />
-    <html>
-    <head>
-        ${helmet.meta.toString()}
-        ${helmet.title.toString()}
-    </head>
-    <body>
-    <div id="app">${html}</div>
-    </body>
-    <script src='public/client_bundle.js'></script>
-    </html>
-      `;
-};
-
-app.get('*', function(req, res) {
+app.get('/', function(req, res) {
   const helmet = Helmet.renderStatic();
   const html = ReactDOMServer.renderToStaticMarkup(<Home />);
-
-  res.send(renderFullPage(html, helmet));
+  res.send(`
+  <!DOCTYPE html />
+  <html>
+  <head>
+      ${helmet.meta.toString()}
+      ${helmet.title.toString()}
+  </head>
+  <body>
+  <div id="app">${html}</div>
+  </body>
+  <script src="/static/client.js"></script>
+  <script src="/static/vendors~client.js"></script>
+  </html>
+    `);
 });
 
 app.listen(PORT, () => console.log('Running in 3000'));
---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const webpack = require('webpack')
+const nodeExternals = require('webpack-node-externals')
+const path = require('path')
+
+const js = { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' }
+
+const serverConfig = {
+  mode: 'development',
+  target: 'node',
+  node: {
+    __dirname: false
+  },
+  externals: [nodeExternals()],
+  entry: {
+    'server.js': path.resolve(__dirname, './server.js')
+  },
+  module: {
+    rules: [js]
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name]'
+  }
+}
+
+const clientConfig = {
+  mode: 'development',
+  target: 'web',
+  entry: {
+    'client.js': path.resolve(__dirname, './client.js')
+  },
+  module: {
+    rules: [js]
+  },
+  optimization: {
+    splitChunks: {
+      chunks: 'all'
+    }
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist/public'),
+    filename: '[name]'
+  }
+}
+
+module.exports = [serverConfig, clientConfig]
