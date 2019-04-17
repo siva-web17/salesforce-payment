6ce6d4febd36db03d8f3390475fab5133d77baea
zural <zural143@gmail.com>
2019-04-17 12:41:06 +0530

    URL parameter are decoded

--- client/Redirection.js
+++ client/Redirection.js
@@ -2,6 +2,14 @@ import React from 'react';
 import { Helmet } from 'react-helmet';
 import { request } from './Request';
 import { payloadDecrytUrl } from './config';
+
+export const getUrlParameterByName = (field, url) => {
+  var href = url ? url : window.location.href;
+  var reg = new RegExp('[?&]' + field + '=([^&#]*)', 'i');
+  var string = reg.exec(href);
+  return string ? string[1] : null;
+};
+
 class Redirection extends React.Component {
   constructor(props) {
     super(props);
@@ -14,7 +22,12 @@ class Redirection extends React.Component {
     );
   };
   componentDidMount() {
-    request({ url: payloadDecrytUrl, name: 'PAYLOAD_DECRYT', header: { bearer: 'xxxxxxx' }, payload: { name: 'some', job: 'some' } });
+    request({
+      url: payloadDecrytUrl,
+      name: 'PAYLOAD_DECRYT',
+      header: { bearer: 'xxxxxxx' },
+      payload: getUrlParameterByName('paydata', decodeURIComponent(window.location.href))
+    });
   }
   render() {
     return (
